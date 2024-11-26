3f16bd0135d8307360b9205603214494cebb5a7a
Kallukunta-indraja <indrajakallukunta@gmail.com>
2024-11-26 16:56:28 +0530

    Prime number program

---
+++ PrimeNumber.java
@@ -0,0 +1,32 @@
+//Write a java program to check whether the given number is prime or not?
+import java.util.Scanner;
+
+public class Prime
+{
+
+	public static void main(String[] args) 
+	{
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num = sc.nextInt();
+		int count=0;
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0) 
+			{
+				count=count+1;
+		    }
+		}
+		if(count==2)
+		{
+			System.out.println(num+" is a prime number");
+		}
+		else
+		{
+			System.out.println(num+" is not a prime number");
+		}
+
+	}
+
+}
