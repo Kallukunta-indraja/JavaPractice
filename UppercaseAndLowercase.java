8ba2db768191e13b8e3f5d02dfec7b652e08c75a
Kallukunta-indraja <indrajakallukunta@gmail.com>
2024-12-01 13:19:12 +0530

    kkk

---
+++ UppercaseAndLowercase.java
@@ -0,0 +1,30 @@
+//Write a java program to toggle characters of given string?
+import java.util.Scanner;
+
+public class UppercaseAndLowercase 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String s = sc.nextLine();
+		for(int i=0;i<s.length();i++) 
+		{
+		 char c = s.charAt(i);
+		if(Character.isUpperCase(c)) 
+		{
+			System.out.println(Character.toLowerCase(c));
+		}
+		else if(Character.isLowerCase(c)) 
+		{
+			System.out.println(Character.toUpperCase(c));
+		}
+		
+		}
+		
+		sc.close();
+
+	}
+
+}
