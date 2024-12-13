5af3f1f154dbb6fdbf400cc0a2017d3e9bd10aea
Kallukunta-indraja <indrajakallukunta@gmail.com>
2024-12-13 22:39:19 +0530

    program to print Right Triangle Star Pattern

---
+++ RightTriangleStarPattern.java
@@ -0,0 +1,21 @@
+//Write a java program to print right triangle star pattern?
+import java.util.Scanner;
+
+public class RightTriangleStarPattern 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of rows: ");
+		int rows=sc.nextInt();
+		for(int i=1;i<=rows;i++) 
+		{
+           for(int j=0;j<i;j++) 
+           {
+        	   System.out.print("*");
+           }
+           System.out.println();
+        }
+		sc.close();
+	}
+}
