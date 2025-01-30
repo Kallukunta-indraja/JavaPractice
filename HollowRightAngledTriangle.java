9fd3f97c3ad83df631b43cc61e78887954f0ae92
Indraja Kallukunta <132640513+Kallukunta-indraja@users.noreply.github.com>
2025-01-30 22:42:45 +0530

    Hollow Right Angled Triangle program

--- HollowRightAngledTriangle.java
+++ HollowRightAngledTriangle.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
-
-class HollowRightAngledTriangle {
-    public static void main(String args[]) {
+class HollowRightAngledTriangle 
+{
+    public static void main(String args[]) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the height of the triangle: ");
         int n = sc.nextInt();
         sc.close();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (j == 1 || j == i || i == n) {
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                if (j == 1 || j == i || i == n) 
+                {
                     System.out.print("*");
-                } else {
+                } 
+                else 
+                {
                     System.out.print(" ");
                 }
             }
